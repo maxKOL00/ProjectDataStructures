3f4c43d9b1ad5cd40be3b1b2a6da4e57cca5ae65
Max Kolanz <mako2910@colorado.edu>
2019-04-15 15:27:30 -0600

    main

--- public/main.js
+++ public/main.js
@@ -1,39 +1,19 @@
 
-//$('.grade-wrapper').on("click", function(e){
-  //e.preventDefault()
-  //$(this).find("div").slideToggle("slow", function () {
-  //});
-//})
-
-// const button1 = document.querySelector("#script1");
-// const button2 = document.querySelector("#script2");
-// const button3 = document.querySelector("#script3");
-// const button4 = document.querySelector("#script4");
-
-// button1.addEventListener("click", () => fetchStartEndpoint(button1.dataset.filename));
-// button2.addEventListener("click", () => fetchStartEndpoint(button2.dataset.filename));
-// button3.addEventListener("click", () => fetchStartEndpoint(button3.dataset.filename));
-// button4.addEventListener("click", () => fetchStartEndpoint(button4.dataset.filename));
-
-
 function fetchStartEndpoint(fileName) {
-  fetch(`/start/${fileName}`)
+  fetch(`/start/${fileName}`)//gets the name of the python script from the function the button calls
     .catch(error => console.log(error));
 }
 
-document.getElementById("meme").style.display = 'none';
+document.getElementById("meme").style.display = 'none';//begin with button hidden
 function writeMemes() {
   var x = document.getElementById("meme");
   if (x.style.display === 'none') {
-        x.style.display = 'inline';
+        x.style.display = 'inline';//toggle display of the button that runs the script
   } else {
     x.style.display = 'none';
     document.getElementById("meme").checked = false;
   }
-  // Uncomment below and change snow to the correct file name you want to trigger in python_scripts
-  // Do the same for all the others below. 
 }
-
 
 document.getElementById("check1").style.display = 'none';
 function check1() {
@@ -44,9 +24,6 @@ function check1() {
     x.style.display = 'none';
     document.getElementById("check1").checked = false;
   }
-   // Uncomment below and change snow to the correct file name you want to trigger in python_scripts
-   // Do the same for all the others below. 
-  fetchStartEndpoint("reddit.py") 
-}
- 
 
+  fetchStartEndpoint("reddit.py") //give the name of the python script to server.js
+}
