f7fd82485fef4ac90d66075999ce9397f975c2bb
Max Kolanz <mako2910@colorado.edu>
2019-04-15 19:15:01 -0600

    added comments to file

--- public/reddit.py
+++ public/reddit.py
@@ -11,17 +11,17 @@ reddit = praw.Reddit(client_id='Tu5oWsGc7cnafw',
 print('Logged into Reddit')
 subreddit = reddit.subreddit('Memes_Of_The_Dank')  # specify which subreddit to read from
 
-hot = subreddit.hot(limit=7)  # get memes from new
+hot = subreddit.hot(limit=7)  # get memes from new, limit shows hwo many 
 
-save_path = 'C:/Users/Max/OneDrive/ds-project/public/'
+save_path = 'C:/Users/Max/OneDrive/ds-project/public/'#path to text file
 
 completeName = os.path.join(save_path, "memes.txt")#put file in public. server can find it only there
-file = open(completeName, "w")
+file = open(completeName, "w")#open the file and write to it
 
 
 def repost_check(comment):
-    for i in word_tokenize(comment):
-        word = i.lower()
+    for i in word_tokenize(comment):#puts comments into an array
+        word = i.lower()#make everything lowercase
         if word == 're' or word == 'ree' or word == 'reee' or word == 'reeee':
             print('flagged word: ', word)
             return True
@@ -31,32 +31,29 @@ def repost_check(comment):
 
     return False
 
-def extract_emojis(str):
+def extract_emojis(str):#since emojis can't be written propely to the file
     return ''.join(c for c in str if c in emoji.UNICODE_EMOJI)
 
 for submission in hot:
-    if not submission.stickied:
+    if not submission.stickied:#posts that always come up first in a subreddit
         title = submission.title
         if not extract_emojis(title): # if title only contained emojis
                 print(title)
         else:
-            print("emoji")
+            print("emoji")#change the title to emoji
         url = submission.url
-        # print('URL: ', url)
         upVotes = submission.ups
-        # print('up votes: ', upVotes)
         time = submission.created_utc
-        # print('time: ', time, '\n')
         comments = submission.comments.list()
         repost = False
         print('Checking comments for signs of a repost...')
         for comment in comments:
-            repost = repost_check(comment.body)
+            repost = repost_check(comment.body)#check the comments for signs of repost
             if repost:
                 print('REPOST')
                 break
 
-        if repost is None:
+        if repost is None:#incase the comments can't be parsed to to unknown character
             print('Error reading boolean', '\n', '\n')
         elif not repost:
             print('Writing to file...', '\n', '\n')
@@ -74,6 +71,6 @@ for submission in hot:
         else:
             print('Do not write post (repost)', '\n')
 
-file.close()
+file.close()#close the memes.txt file
 
-os.system("C:/Users/Max/OneDrive/ds-project/public/sort.exe")
+os.system("C:/Users/Max/OneDrive/ds-project/public/sort.exe")#sorts the memes and writes the html file
